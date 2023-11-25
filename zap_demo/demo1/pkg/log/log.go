c0bcdd6843618591eee1929fc7e53b53f38ebaef
allen <allenshaw19@gmail.com>
2023-11-25 14:10:25 +0800

    add demo

---
+++ zap_demo/demo1/pkg/log/log.go
@@ -0,0 +1,192 @@
+package log
+
+import (
+	"io"
+	"os"
+	"time"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type Level = zapcore.Level
+
+const (
+	InfoLevel   Level = zap.InfoLevel   // 0, default level
+	WarnLevel   Level = zap.WarnLevel   // 1
+	ErrorLevel  Level = zap.ErrorLevel  // 2
+	DPanicLevel Level = zap.DPanicLevel // 3, used in development log
+	// PanicLevel logs a message, then panics
+	PanicLevel Level = zap.PanicLevel // 4
+	// FatalLevel logs a message, then calls os.Exit(1).
+	FatalLevel Level = zap.FatalLevel // 5
+	DebugLevel Level = zap.DebugLevel // -1
+)
+
+type Field = zap.Field
+
+func (l *Logger) Debug(msg string, fields ...Field) {
+	l.l.Debug(msg, fields...)
+}
+
+func (l *Logger) Info(msg string, fields ...Field) {
+	l.l.Info(msg, fields...)
+}
+
+func (l *Logger) Warn(msg string, fields ...Field) {
+	l.l.Warn(msg, fields...)
+}
+
+func (l *Logger) Error(msg string, fields ...Field) {
+	l.l.Error(msg, fields...)
+}
+func (l *Logger) DPanic(msg string, fields ...Field) {
+	l.l.DPanic(msg, fields...)
+}
+func (l *Logger) Panic(msg string, fields ...Field) {
+	l.l.Panic(msg, fields...)
+}
+func (l *Logger) Fatal(msg string, fields ...Field) {
+	l.l.Fatal(msg, fields...)
+}
+
+// function variables for all field types
+// in github.com/uber-go/zap/field.go
+
+var (
+	String     = zap.String
+	Skip       = zap.Skip
+	Binary     = zap.Binary
+	Bool       = zap.Bool
+	Boolp      = zap.Boolp
+	ByteString = zap.ByteString
+	Float64    = zap.Float64
+	Float64p   = zap.Float64p
+	Float32    = zap.Float32
+	Float32p   = zap.Float32p
+	Durationp  = zap.Durationp
+	Any        = zap.Any
+	Int        = zap.Int
+
+	Info   = std.Info
+	Warn   = std.Warn
+	Error  = std.Error
+	DPanic = std.DPanic
+	Panic  = std.Panic
+	Fatal  = std.Fatal
+	Debug  = std.Debug
+)
+
+// not safe for concurrent use
+func ResetDefault(l *Logger) {
+	std = l
+}
+
+type Logger struct {
+	l     *zap.Logger // zap ensure that zap.Logger is safe for concurrent use
+	level Level
+}
+
+var std = New(os.Stderr, InfoLevel)
+
+func Default() *Logger {
+	return std
+}
+
+type Option = zap.Option
+
+var (
+	WithCaller    = zap.WithCaller
+	AddStacktrace = zap.AddStacktrace
+)
+
+// New create a new logger (not support log rotating).
+func New(writer io.Writer, level Level, opts ...Option) *Logger {
+	if writer == nil {
+		panic("the writer is nil")
+	}
+	cfg := zap.NewProductionConfig()
+	cfg.EncoderConfig.EncodeTime = func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+		enc.AppendString(t.Format("2006-01-02T15:04:05.000Z0700"))
+	}
+	cfg.EncoderConfig.FunctionKey = "func"
+
+	// 自定义时间输出格式
+	customTimeEncoder := func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+		enc.AppendString(t.Format("2006-01-02 15:04:05.000") + "|")
+	}
+	// 自定义日志级别显示
+	customLevelEncoder := func(level zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
+		enc.AppendString(level.CapitalString() + "|")
+	}
+
+	// 自定义文件：行号输出项
+	customCallerEncoder := func(caller zapcore.EntryCaller, enc zapcore.PrimitiveArrayEncoder) {
+		enc.AppendString(caller.TrimmedPath() + "|")
+	}
+
+	cfg.EncoderConfig.EncodeTime = customTimeEncoder
+	cfg.EncoderConfig.EncodeLevel = customLevelEncoder
+	cfg.EncoderConfig.EncodeCaller = customCallerEncoder
+
+	core := zapcore.NewCore(
+		zapcore.NewConsoleEncoder(cfg.EncoderConfig),
+		zapcore.AddSync(writer),
+		zapcore.Level(level),
+	)
+
+	opts = append(opts, zap.AddCaller(), zap.AddCallerSkip(1))
+	logger := &Logger{
+		l:     zap.New(core, opts...),
+		level: level,
+	}
+	return logger
+}
+
+func (l *Logger) Sync() error {
+	return l.l.Sync()
+}
+
+func Sync() error {
+	if std != nil {
+		return std.Sync()
+	}
+	return nil
+}
+
+type LevelEnablerFunc func(lvl Level) bool
+
+type TeeOption struct {
+	W   io.Writer
+	Lef LevelEnablerFunc
+}
+
+func NewTee(tops []TeeOption, opts ...Option) *Logger {
+	var cores []zapcore.Core
+	cfg := zap.NewProductionConfig()
+	cfg.EncoderConfig.EncodeTime = func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+		enc.AppendString(t.Format("2006-01-02T15:04:05.000Z0700"))
+	}
+	for _, top := range tops {
+		top := top
+		if top.W == nil {
+			panic("the writer is nil")
+		}
+
+		lv := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+			return top.Lef(Level(lvl))
+		})
+
+		core := zapcore.NewCore(
+			zapcore.NewJSONEncoder(cfg.EncoderConfig),
+			zapcore.AddSync(top.W),
+			lv,
+		)
+		cores = append(cores, core)
+	}
+
+	logger := &Logger{
+		l: zap.New(zapcore.NewTee(cores...), opts...),
+	}
+	return logger
+}
