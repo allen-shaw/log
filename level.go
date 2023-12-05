8c1fc4102e8b6a739292a242be0dd180e2c4ba36
xin.xiao1 <xin.xiao@seamoney.com>
2023-12-05 21:08:37 +0800

    fix(log): remove unuse level

--- level.go
+++ level.go
@@ -5,12 +5,11 @@ import "go.uber.org/zap/zapcore"
 type Level = zapcore.Level
 
 const (
-	unknownLevel = zapcore.DebugLevel - 1
-	DebugLevel   = zapcore.DebugLevel
-	InfoLevel    = zapcore.InfoLevel
-	WarnLevel    = zapcore.WarnLevel
-	ErrorLevel   = zapcore.ErrorLevel
-	DPanicLevel  = zapcore.DPanicLevel
-	PanicLevel   = zapcore.PanicLevel
-	FatalLevel   = zapcore.FatalLevel
+	DebugLevel  = zapcore.DebugLevel
+	InfoLevel   = zapcore.InfoLevel
+	WarnLevel   = zapcore.WarnLevel
+	ErrorLevel  = zapcore.ErrorLevel
+	DPanicLevel = zapcore.DPanicLevel
+	PanicLevel  = zapcore.PanicLevel
+	FatalLevel  = zapcore.FatalLevel
 )
